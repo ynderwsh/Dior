7bae4c544ff1dd8b6b6a686fe8590993f50f28f0
Andrew <shitovandrey@list.ru>
2024-12-29 00:41:11 +0500

    origin main

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper('.swiper', {
+    
+    loop: true,
+    
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
