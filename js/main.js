62296154f3f8e06de7a4aefc76655eed853847be
Andrew <shitovandrey@list.ru>
2024-12-29 23:52:00 +0500

    first commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
-const swiper = new Swiper('.swiper', {
+const swiper = new Swiper('.top__swiper', {
     
-    loop: true,
+    effect: 'fade',
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction:false,
+    },
     
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+  });
+
+
+
+  const swiperAbout = new Swiper(".about__slider", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    freeMode: true,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
