2a17871766482f359d8e85ea2c268fe716804e52
Andrew <shitovandrey@list.ru>
2025-01-02 00:01:36 +0500

    third commit

--- js/main.js
+++ js/main.js
@@ -25,3 +25,9 @@ const swiper = new Swiper('.top__swiper', {
     },
   
   });
+
+  document.querySelectorAll('.accordeon__triger').forEach((item)=>{
+    item.addEventListener('click', () => {
+      item.parentNode.classList.toggle('accordeon__item--active')
+    })
+  });
